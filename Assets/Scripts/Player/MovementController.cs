1239f4d476d48b4e7817391ba0623184bd3ce4c3
emrecancorapci <emrecancorapci@gmail.com>
2022-05-02 14:26:41 +0300

    Controller improved

--- Assets/Scripts/Player/MovementController.cs
+++ Assets/Scripts/Player/MovementController.cs
@@ -7,17 +7,18 @@ namespace Player
 {
     public class MovementController : MonoBehaviour
     {
+        private const double BottomFlagLength = 0.2;
+
         [SerializeField]
         private float MovementSpeed;
         [SerializeField]
         private float MovementRotationSpeed;
         [SerializeField]
-        private float RotationSpeed;
+        private float bodyRotationSpeed;
         [SerializeField]
         private float JumpForce;
         [SerializeField]
         private float ShiftBoost;
-        
         
         private InputController input;
         private Rigidbody body;
@@ -26,20 +27,11 @@ namespace Player
 
         private Vector3 movementVector = Vector3.zero;
         private Vector2 movementInput;
-        private bool _isGround;
 
         private bool isRunning => movementInput.magnitude != 0;
-
-        private bool IsGround
-        {
-            get => _isGround;
-            set
-            {
-                Debug.Log("IsGround is " + _isGround); 
-                _isGround = value;
-            }
-        }
-
+        private bool isGround;
+        private bool isWalkable;
+        
         private static readonly int BIsRunning = Animator.StringToHash("b_isRunning");
         private static readonly int BIsJumping = Animator.StringToHash("b_isJumping");
         private static readonly int BIsSprinting = Animator.StringToHash("b_isSprinting");
@@ -52,37 +44,34 @@ namespace Player
             
             input = new InputController();
             input.Enable();
-            
-            input.Player.WASD.performed += context => movementInput = context.ReadValue<Vector2>();
-            input.Player.Space.performed += _ => Jump();
-            input.Player.Shift.started += _ =>
-            {
-                animator.SetBool(BIsSprinting, true);
-                MovementSpeed += ShiftBoost;
-            };
-            input.Player.Shift.canceled += _ =>
-            {
-                animator.SetBool(BIsSprinting, false);
-                MovementSpeed -= ShiftBoost;
-            };
-        }
-        
-        private void Update()
-        {
+
+            InputHandler();
             AnimationHandler();
         }
-
-        void FixedUpdate()
+        private void InputHandler()
         {
-            PlayerRotation();
-            PlayerMovement();
+            Debug.Log("InputHandler created.");
+            input.Player.WASD.performed += context => movementInput = context.ReadValue<Vector2>();
+            input.Player.Shift.started += _ => MovementSpeed += ShiftBoost;
+            input.Player.Shift.canceled += _ => MovementSpeed -= ShiftBoost;
+            input.Player.Space.started += _ =>
+            {
+                if (isGround || isWalkable) PlayerJump();
+            };
         }
-
         private void AnimationHandler()
         {
-            animator.SetBool(BIsRunning, isRunning);
+            input.Player.WASD.performed += _ => animator.SetBool(BIsRunning, isRunning); Debug.Log("WASD");
+            input.Player.Shift.performed += _ => animator.SetBool(BIsSprinting, true);
+            input.Player.Shift.canceled += _ => animator.SetBool(BIsSprinting, false);
+            input.Player.Space.performed += _ =>
+            { if (isGround || isWalkable) animator.SetBool(BIsJumping, true); };
         }
-
+        private void FixedUpdate()
+        {
+            PlayerMovement();
+            PlayerRotation();
+        }
         private void PlayerMovement()
         {
             Vector3 targetVector = (camTransform.forward * movementInput.y
@@ -92,7 +81,6 @@ namespace Player
                                            Time.fixedDeltaTime * MovementRotationSpeed);
             transform.position += movementVector * (MovementSpeed * Time.fixedDeltaTime);
         }
-
         private void PlayerRotation()
         {
             float targetX = camTransform.forward.x * movementInput.y + camTransform.right.x * movementInput.x;
@@ -100,41 +88,41 @@ namespace Player
             Vector3 targetDirection = new Vector3(targetX, 0, targetZ).normalized;
             
             transform.forward = Vector3.Slerp(transform.forward, targetDirection,
-                                             RotationSpeed * Time.fixedDeltaTime);
+                                             bodyRotationSpeed * Time.fixedDeltaTime);
         }
-
-        private void Jump()
-        {
-            if (!IsGround) return;
-            
-            animator.SetBool(BIsJumping, true);
-            body.AddForce(Vector3.up * JumpForce * body.mass, ForceMode.Impulse);
-        }
-
+        
+        private void PlayerJump() => body.AddForce(Vector3.up * JumpForce * body.mass, ForceMode.Impulse);
+        
         private void OnCollisionEnter(Collision other)
         {
-            Debug.Log("Col with : " + other.gameObject.tag);
+            Debug.Log("Col enter : " + other.gameObject.tag);
+
+            if (other.gameObject.CompareTag("Ground"))
+            {
+                isGround = true;
+                animator.SetBool(BIsJumping, false);
+            }
             
-            CheckGround(other);
+            else if (other.gameObject.CompareTag("Walkable"))
+            {
+                bool isLandedOn = other.contacts
+                    .Any(contactPoint => contactPoint.point.y < transform.position.y + BottomFlagLength);
+                if (!isLandedOn) return;
+                
+                isWalkable = true;
+                animator.SetBool(BIsJumping, false);
+            }
+            
         }
         
         private void OnCollisionExit(Collision other)
         {
+            Debug.Log("Col exit : " + other.gameObject.tag);
             if (other.gameObject.CompareTag("Ground"))
-            {
-                IsGround = false;
-            }
-        }
+                isGround = false;
 
-        private void CheckGround(Collision colliding)
-        {
-            if (!colliding.gameObject.CompareTag("Ground")) return;
-            
-            if (colliding.contacts.Any(contactPoint => contactPoint.point.y < transform.position.y))
-            {
-                IsGround = true;
-                animator.SetBool(BIsJumping, false);
-            }
+            else if (other.gameObject.CompareTag("Walkable"))
+                isWalkable = false;
         }
     }
 }
